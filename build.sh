0869b4d6dd82773b8eb7e35ea30e70a81cf624a7
NahidI11 <111808023+NahidI11@users.noreply.github.com>
2022-10-28 14:55:11 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-echo "build app..."
-
-cd /home/jenkins/.jenkins/workspace/qa-project
-
-sudo chown "$USER":"$USER" /var/run/docker.sock -R
-sudo chmod g+rwx "/var/run/docker.sock" -R
-
-docker build -t myapp .
+echo "setup stage"
+docker-compose build
